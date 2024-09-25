318abed0a4644898432289c5c4927f9804d38148
nannis36 <nick.w.annis@gmail.com>
2024-09-25 03:36:13 -0600

    Update DirectedAcyclicGraph.java
    Found a bug with trying to return the list of nodes leading to the longest path. Interpreting that list as a solution led to an incorrect solution from the algorithm so it's been removed and a simpler answer was returned instead.

--- DirectedAcyclicGraph.java
+++ DirectedAcyclicGraph.java
@@ -1,23 +1,25 @@
 package mindBankDAG;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
 public class DirectedAcyclicGraph extends Graph {
+	
 	public void connectVertices(Vertex parentVertex, Vertex childVertex) {
 		parentVertex.addChildVertex(childVertex);
 	}
 	
 	private void topologicalSort(Vertex v, Map<Integer,Boolean> visited, Stack<Vertex> stack){
+		
 		// Mark current vertex as visited
 		visited.put(v.getId(), true);
 		
 		// Traverse all child vertices
 		for(Map.Entry<Integer,Vertex> childVertexMapEntry : v.getChildVertices().entrySet()) {
+			
 			Vertex child = childVertexMapEntry.getValue();
+			
 			if(!visited.get(child.getId())) {
 				topologicalSort(child,visited,stack);
 			}
@@ -27,14 +29,15 @@ public class DirectedAcyclicGraph extends Graph {
 		stack.push(v);
 	}
 	
-	public List<Vertex> findLongestPathFromVertex(Vertex sourceVertex){
+	public Map<String,Integer> findLongestPathFromVertex(Vertex sourceVertex){
+		
 		// Initialize stack for topological sorting
 		Stack<Vertex> stack = new Stack<Vertex>();
-		Map<Integer,Integer> dist = new HashMap<Integer,Integer>();
+
+		// Initialize a map to store distances to every vertex
+		Map<Integer,Integer> distanceMap = new HashMap<Integer,Integer>();
 		
-		// initialize map to track path taken to a given vertex
-		Map<Integer,List<Vertex>> paths = new HashMap<Integer,List<Vertex>>();
-		
+		// Initialize map to track visited vertices by their id
 		Map<Integer,Boolean> visited = new HashMap<Integer,Boolean>();
 		
 		// Mark all vertices as not visited
@@ -43,36 +46,45 @@ public class DirectedAcyclicGraph extends Graph {
 		}
 		
 		// Call topologicalSort to store sort values from all vertices
+		// Traverse all unvisited vertices and call topological sort
 		for(Map.Entry<Integer,Boolean> visitedEntry : visited.entrySet()) {
+			
 			if(visitedEntry.getValue() == false) {
 				Vertex startFrom = getVertex(visitedEntry.getKey());
 				topologicalSort(startFrom, visited, stack);
 			}
-		}
-		
-		// Initialize distances to all vertices as infinite and the distance to the source as 0
-		for(Map.Entry<Integer,Vertex> distanceEntry : vertices.entrySet()) {
-			Vertex distanceVertex = distanceEntry.getValue();
-			int vid = distanceVertex.getId();
-			dist.put(vid,Integer.MIN_VALUE);
-			paths.put(vid, new ArrayList<Vertex>());
-			paths.get(vid).add(sourceVertex); //initialize path with starting vertex
-		}
-		
-		dist.put(sourceVertex.getId(),0);
-		
-		// Process vertices in topoligical order
-		while(!stack.isEmpty()) {
-			Vertex nextV = stack.pop();
 			
-			if(dist.get(nextV.getId()) != Integer.MIN_VALUE) {
-				Map<Integer,Vertex> childVertices = nextV.getChildVertices();
+		}
+		
+		// Initialize distances to all vertices as a minimum value, marking them as inaccessible at this point in the algorithm
+		for(Map.Entry<Integer,Vertex> distanceEntry : vertices.entrySet()) {
+			
+			Vertex distanceVertex = distanceEntry.getValue();
+
+			int vid = distanceVertex.getId();
+			distanceMap.put(vid,Integer.MIN_VALUE);
+			
+		}
+		
+		// Set the starting vertex distance to 0
+		distanceMap.put(sourceVertex.getId(),0);
+		
+		// Process vertices in topological order
+		while(!stack.isEmpty()) {
+			
+			Vertex nextStackVertex = stack.pop();
+			
+			// Update distances of adjacent vertices
+			if(distanceMap.get(nextStackVertex.getId()) != Integer.MIN_VALUE) {
+				
+				Map<Integer,Vertex> childVertices = nextStackVertex.getChildVertices();
+				
 				for(Map.Entry<Integer,Vertex> nextChildEntry : childVertices.entrySet()) {	
+					
 					Vertex childVertex = nextChildEntry.getValue();
-					if(dist.get(childVertex.getId()) <= dist.get(nextV.getId()) ) {
-						int vid = childVertex.getId();
-						dist.put(vid, dist.get(nextV.getId()) + 1);
-						paths.get(vid).add(nextV);
+					
+					if(distanceMap.get(childVertex.getId()) <= distanceMap.get(nextStackVertex.getId()) ) {
+						distanceMap.put( childVertex.getId() , distanceMap.get(nextStackVertex.getId()) + 1);
 					}
 				}
 			}
@@ -81,22 +93,24 @@ public class DirectedAcyclicGraph extends Graph {
 		//loop through saved distance values to find the longest
 		int longest = Integer.MIN_VALUE;
 		Integer longestId = null;
-		for(Map.Entry<Integer,Integer> distances : dist.entrySet()) {
+		for(Map.Entry<Integer,Integer> distances : distanceMap.entrySet()) {
 			Integer lid = distances.getKey();
 			int distance = distances.getValue();
-			paths.get(lid).add(getVertex(lid)); //path does not have the destination vertex yet, add it here
 			if(distance > longest) {
 				longest = distance;
 				longestId = lid;
 			}
 		}
 		
-		//return list of vertices for the longest path
-		return paths.get(longestId);
+		Map<String,Integer> returnMap = new HashMap<String,Integer>();
+		returnMap.put("longestId", longestId);
+		returnMap.put("distance", longest);
+		
+		return returnMap;
 		
 	}
 	
-	public List<Vertex> findLongestPathFromVertex(int id){
+	public Map<String,Integer> findLongestPathFromVertex(int id){
 		return findLongestPathFromVertex(getVertex(id));
 	}
 }
